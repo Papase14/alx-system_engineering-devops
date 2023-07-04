4ecf76ffabc36687c5659067e126ee4062c24346
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-07-04 23:37:06 +0200

    2. Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# Script matched string with h followed by 0 or 1 occurence of b
-# then tn
+# Script matched string with h followed by 0 or 1 occurence of b then tn
 
 puts ARGV[0].scan(/hb?tn/).join
