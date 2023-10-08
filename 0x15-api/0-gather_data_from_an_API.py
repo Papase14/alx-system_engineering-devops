892351d931cca560ef9da3856adbc5f61a08a3c2
Tshupane Morake <tshupane@outlook.com>
2023-10-08 21:45:21 +0200

    back

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,57 +1,42 @@
 #!/usr/bin/python3
+"""script for parsing web data from an api
 """
-script for parsing web data from an api
-"""
-import json
 import requests
 import sys
 
 
-def fetch_user_info(userId):
-    # create Response object for specific user
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
-    user_response = requests.get(user_url)
+def get_employee_todo_list_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    if user_response.status_code != 200:
-        print("Error fetching user data.")
-        sys.exit(1)
+    # Fetch user info
+    user_url = f"{base_url}/users/{employee_id}"
+    response = requests.get(user_url)
+    user_data = response.json()
 
-    user_info = json.loads(user_response.text)
-    return user_info
+    # Fetch user's todos
+    todos_url = f"{base_url}/todos?userId={employee_id}"
+    response = requests.get(todos_url)
+    todos_data = response.json()
 
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = sum(1 for todo in todos_data if todo["completed"])
 
-def fetch_user_tasks(userId):
-    # create Response object for user's tasks
-    tasks_url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(userId)
-    tasks_response = requests.get(tasks_url)
+    # Print progress information
+    print(
+        f'Employee {user_data["name"]} is done with' +
+        f'tasks({completed_tasks}/{total_tasks}):'
+    )
 
-    if tasks_response.status_code != 200:
-        print("Error fetching user's tasks data.")
-        sys.exit(1)
-
-    tasks_info = json.loads(tasks_response.text)
-    return tasks_info
+    for todo in todos_data:
+        if todo["completed"]:
+            print(f'\t{todo["title"]}')
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <userId>")
+        print("Usage: python script_name.py <employee_id>")
         sys.exit(1)
 
-    userId = sys.argv[1]
-    user_info = fetch_user_info(userId)
-    tasks_info = fetch_user_tasks(userId)
-
-    employee_name = user_info["name"]
-    num_done = sum(1 for task in tasks_info if task["completed"])
-    num_tasks = len(tasks_info)
-
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name, num_done, num_tasks
-        )
-    )
-
-    for task in tasks_info:
-        if task["completed"]:
-            print("\t {}".format(task["title"]))
+    employee_id = int(sys.argv[1])
+    get_employee_todo_list_progress(employee_id)
