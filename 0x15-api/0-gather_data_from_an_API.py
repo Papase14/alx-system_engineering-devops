e3156eb79c68c1e7f111ce01f78c0e49a9674272
Tshupane Morake <tshupane@outlook.com>
2023-10-08 21:44:00 +0200

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,27 +7,44 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    num_done, num_tasks = 0, 0
-    userId = sys.argv[1]
+def fetch_user_info(userId):
+    # create Response object for specific user
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
+    user_response = requests.get(user_url)
 
-    # create Response object for specific user and that user's tasks
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(userId)
-    user_response = requests.get(url)
+    if user_response.status_code != 200:
+        print("Error fetching user data.")
+        sys.exit(1)
 
-    url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(userId)
-    todo_response = requests.get(url)
-
-    # create Dictionary objects from response objects
     user_info = json.loads(user_response.text)
-    todo_info = json.loads(todo_response.text)
+    return user_info
+
+
+def fetch_user_tasks(userId):
+    # create Response object for user's tasks
+    tasks_url = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(userId)
+    tasks_response = requests.get(tasks_url)
+
+    if tasks_response.status_code != 200:
+        print("Error fetching user's tasks data.")
+        sys.exit(1)
+
+    tasks_info = json.loads(tasks_response.text)
+    return tasks_info
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <userId>")
+        sys.exit(1)
+
+    userId = sys.argv[1]
+    user_info = fetch_user_info(userId)
+    tasks_info = fetch_user_tasks(userId)
 
     employee_name = user_info["name"]
-
-    for task in todo_info:
-        num_tasks += 1
-        if task["completed"]:
-            num_done += 1
+    num_done = sum(1 for task in tasks_info if task["completed"])
+    num_tasks = len(tasks_info)
 
     print(
         "Employee {} is done with tasks({}/{}):".format(
@@ -35,6 +52,6 @@ if __name__ == "__main__":
         )
     )
 
-    for task in todo_info:
+    for task in tasks_info:
         if task["completed"]:
             print("\t {}".format(task["title"]))
