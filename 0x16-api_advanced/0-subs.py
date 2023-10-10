43f06d46eb39fa309ac417cfdfb18e3e4bb49d03
Tshupane Morake <tshupane@outlook.com>
2023-10-10 23:00:42 +0200

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-'''
-API to REDDIT
-'''
+"""
+    This module contains a function that queries the
+    Reddit API and returns the number of subscribers
+    for a given subreddit.
+"""
 
-reddit_url = ""
+import requests
 
-def number_of_subscribers(subreddit):
-    
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the given subreddit.
+    """
+    headers = {"User-Agent": "/u/Suspicious-Jelly920"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+    else:
+        subscribers = 0
+    return subscribers
