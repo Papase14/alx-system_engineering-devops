66a61e06472b440c878889c7d24ac14f8d206e15
Tshupane Morake <tshupane@outlook.com>
2023-10-11 00:49:24 +0200

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -15,18 +15,26 @@ def recurse(
     Returns a list of titles of all hot posts on a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit to retrieve hot posts from.
-        hot_list (List[str], optional): A list to store the titles of the hot posts (default is an empty list).
-        after (str, optional): The ID of the last post in the previous batch (default is an empty string).
-        count (int, optional): The total number of posts retrieved so far (default is 0).
+        - subreddit (str): The name of the subreddit to retrieve hot
+        posts from.
+        - hot_list (List[str], optional): A list to store the titles
+        of the hot posts (default is an empty list).
+        - after (str, optional): The ID of the last post in the previous
+        batch (default is an empty string).
+        - count (int, optional): The total number of posts retrieved so
+        far (default is 0).
 
     Returns:
-        List[str]: A list of strings containing the titles of all hot posts on the given subreddit.
+        List[str]: A list of strings containing the titles of all hot
+        posts on the given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {"User-Agent": "/u/Suspicious-Jelly920"}
     params = {"after": after, "count": count, "limit": 100}
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
     if response.status_code == 404:
         return None
